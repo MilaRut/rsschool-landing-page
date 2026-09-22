e3d1e8fbd3c73fcc3f7858bfacb8dc430645f020
Mila Balashova <mila.balashova2312@gmail.com>
2026-09-23 00:21:55 +0300

    feat: update scripts for menu and theme switch

--- assets/js/modules/theme.js
+++ assets/js/modules/theme.js
@@ -1,30 +1,12 @@
 const themeToggle = document.querySelector('.header__theme-switcher');
-const imgLight = document.querySelectorAll('.light');
-const imgDark = document.querySelectorAll('.dark');
-
-function show(arr) {
-  arr.forEach((el) => {
-    el.style.display = 'block';
-  })
-}
-
-function hide(arr) {
-  arr.forEach((el) => {
-    el.style.display = 'none';
-  })
-}
 
 function loadCurrentTheme() {
   const currentTheme = localStorage.getItem('data-theme') || '';
   if (currentTheme === 'light') {
     document.body.classList.add('light');
-    hide(imgLight);
-    show(imgDark);
   } else {
     if (document.body.classList.contains('light')) {
       document.body.classList.remove('light');
-      hide(imgDark);
-      show(imgLight);
     }
   }
 }
@@ -35,15 +17,11 @@ function switchTheme() {
     if (currentTheme === '') {
       document.body.classList.add('light');
       localStorage.setItem('data-theme', 'light');
-      hide(imgLight);
-      show(imgDark);
     } else {
       if (document.body.classList.contains('light')) {
         document.body.classList.remove('light');
       }
       localStorage.setItem('data-theme', '')
-      hide(imgDark);
-      show(imgLight);
     }
   });
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-import {toggleMenu} from "./modules/menu.js";
+import {toggleMenu, mediaQuery, handleMediaChange} from "./modules/menu.js";
 import {scrollToTop, showButton} from "./modules/top-btn.js";
 import {loadCurrentTheme, switchTheme} from "./modules/theme.js";
 import {setHeaderOnScroll} from "./modules/header.js";
@@ -10,4 +10,5 @@ window.addEventListener('DOMContentLoaded', () => {
   showButton();
   switchTheme();
   setHeaderOnScroll();
+  mediaQuery.addEventListener('change', handleMediaChange);
 });
